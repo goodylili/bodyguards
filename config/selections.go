000653d25efe2c18c93b925b3624462e814cafd4
Goodnessuc <ukejegoodness599@gmail.com>
2023-05-30 22:09:47 +0100

    updated the selections file to return required arguments

--- config/selections.go
+++ config/selections.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
@@ -14,7 +13,7 @@ type Config struct {
 	} `yaml:"run"`
 }
 
-func LoadSelections() {
+func LoadSelections() (string, []string) {
 	filename := "bodyguards.yaml"
 	yamlContent, err := os.ReadFile(filename)
 	if err != nil {
@@ -27,9 +26,5 @@ func LoadSelections() {
 		log.Fatalf("Failed to unmarshal YAML: %v", err)
 	}
 
-	fmt.Printf("Architecture: %s\n", config.Run.Architecture)
-	fmt.Println("Enabled features:")
-	for _, feature := range config.Run.Enable {
-		fmt.Println(feature)
-	}
+	return config.Run.Architecture, config.Run.Enable
 }
